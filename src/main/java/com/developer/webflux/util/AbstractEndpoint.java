7dc8bd07659768931b6d4a9d7ca469269e3f940f
trongdv <trongdv@smartosc.com>
2021-05-07 01:56:31 +0700

    Remove comment

--- src/main/java/com/developer/webflux/util/AbstractEndpoint.java
+++ src/main/java/com/developer/webflux/util/AbstractEndpoint.java
@@ -1,31 +1,11 @@
-/*
- * OTATALK - https://otatalk.net/
- * Copyright (C) 2019
- * See README.md for more information
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package com.developer.webflux.util;
 
 import com.developer.webflux.constant.EmployeeConstant;
 import com.developer.webflux.dto.EmployeeAPIResponseEntity;
 import com.developer.webflux.dto.ErrorDetails;
 import com.developer.webflux.exception.CustomException;
-import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
-import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;

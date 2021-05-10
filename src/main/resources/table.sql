43d0cf97cdf58d1223ceb74c485e4131d5d58237
trongdv <trongdv@smartosc.com>
2021-05-11 00:19:35 +0700

    update code

---
+++ src/main/resources/table.sql
@@ -0,0 +1,7 @@
+create table employee (
+    id char(36) default (uuid()) not null primary key,
+    username varchar(20),
+    full_name varchar(255),
+    date_of_birth varchar(255),
+    age integer
+)
